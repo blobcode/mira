c180946ccb0b222da5b76a3a4211d4b7e5a414e0
blobcode <lhelme123@gmail.com>
2021-12-10 09:45:13 -0500

    revamped everything

--- src/client.rs
+++ src/client.rs
@@ -1,26 +1,59 @@
-use serde::{Deserialize, Serialize};
-use std::io::Write;
+use std::io::{self, ErrorKind, Read, Write};
 use std::net::TcpStream;
+use std::sync::mpsc::{self, TryRecvError};
+use std::thread;
+use std::time::Duration;
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-struct Message {
-    name: String,
-    message: String,
-}
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 1024;
 
 pub fn main() {
-    if let Ok(mut stream) = TcpStream::connect("127.0.0.1:8080") {
-        println!("Connected to the server!");
+    let mut client = TcpStream::connect(LOCAL).expect("Stream failed to connect");
+    client
+        .set_nonblocking(true)
+        .expect("failed to initiate non-blocking");
 
-        let tosend = Message {
-            name: "me 1".to_string(),
-            message: "testmessage".to_string(),
-        };
+    let (tx, rx) = mpsc::channel::<String>();
 
-        let encoded: Vec<u8> = bincode::serialize(&tosend).unwrap();
+    thread::spawn(move || loop {
+        let mut buff = vec![0; MSG_SIZE];
+        match client.read_exact(&mut buff) {
+            Ok(_) => {
+                let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+                println!("recv > {}", msg);
+            }
+            Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+            Err(_) => {
+                println!("connection with server was severed");
+                break;
+            }
+        }
 
-        stream.write_all(&encoded);
-    } else {
-        println!("Couldn't connect to server...");
+        match rx.try_recv() {
+            Ok(msg) => {
+                let mut buff = msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+                client.write_all(&buff).expect("writing to socket failed");
+                //println!("me > {}", msg);
+            }
+            Err(TryRecvError::Empty) => (),
+            Err(TryRecvError::Disconnected) => break,
+        }
+
+        thread::sleep(Duration::from_millis(100));
+    });
+
+    println!("[ mira ]");
+    loop {
+        let mut buff = String::new();
+        io::stdin()
+            .read_line(&mut buff)
+            .expect("reading from stdin failed");
+        let msg = buff.trim().to_string();
+        if msg == ":quit" || tx.send(msg).is_err() {
+            break;
+        }
     }
+    println!("bye bye!");
 }
--- src/server.rs
+++ src/server.rs
@@ -1,33 +1,77 @@
-use serde::{Deserialize, Serialize};
-use std::io::Read;
-use std::net::{TcpListener, TcpStream};
+use std::io::{ErrorKind, Read, Write};
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use std::sync::mpsc;
+use std::thread;
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
-struct Message {
-    name: String,
-    message: String,
+use crate::client;
+
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 1024;
+
+fn sleep() {
+    thread::sleep(::std::time::Duration::from_millis(100));
 }
 
-pub fn handle_connection(mut stream: TcpStream) {
-    // handle ze connection
-    let mut rx_bytes = [0u8; 1000];
-    // Read from the current data in the TcpStream
-    stream.read(&mut rx_bytes);
-    let decoded: Message = bincode::deserialize(&rx_bytes[..]).unwrap();
-    println!("{} > {}", decoded.name, decoded.message);
+struct Message {
+    addr: SocketAddr,
+    msg: String,
 }
 
 pub fn main() {
-    println!("running server");
+    let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
+    server
+        .set_nonblocking(true)
+        .expect("failed to initialize non-blocking");
 
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let mut clients = vec![];
+    let (tx, rx) = mpsc::channel::<Message>();
+    loop {
+        if let Ok((mut socket, addr)) = server.accept() {
+            println!("Client {} connected", addr);
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_connection(stream);
-            }
-            Err(e) => { /* connection failed */ }
+            let tx = tx.clone();
+            clients.push(socket.try_clone().expect("failed to clone client"));
+
+            thread::spawn(move || loop {
+                let mut buff = vec![0; MSG_SIZE];
+
+                match socket.read_exact(&mut buff) {
+                    Ok(_) => {
+                        // parse incoming message
+                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+
+                        println!("{}: {:?}", addr, msg);
+
+                        // create message struct
+                        let msgt = Message { addr, msg };
+
+                        // send message to threads
+                        tx.send(msgt).expect("failed to send msg to threads");
+                    }
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(_) => {
+                        println!("closing connection with: {}", addr);
+                        break;
+                    }
+                }
+
+                sleep();
+            });
         }
+
+        // on thread recive message
+        if let Ok(msg) = rx.try_recv() {
+            for mut client in &clients {
+                let mut buff = msg.msg.clone().into_bytes();
+                buff.resize(MSG_SIZE, 0);
+
+                if client.peer_addr().unwrap() != msg.addr {
+                    client.write_all(&buff).map(|_| client).unwrap();
+                };
+            }
+        }
+
+        sleep();
     }
 }
